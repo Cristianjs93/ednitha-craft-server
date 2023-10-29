1c9d32d934d79a2f37f31e7ac9adb73599e4c14a
Cristian <cristian.jimenezsa@hotmail.com>
2023-10-28 20:38:39 -0500

    editor config

--- src/api/healthcheck/healthcheck.controller.ts
+++ src/api/healthcheck/healthcheck.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
 export const healthcheckHandler = (_: Request, res: Response) => {
-  res.status(200).json({ message: 'Server ok' });
+  res.status(200).json({ message: 'Server ok', uptime: process.uptime() });
 };
